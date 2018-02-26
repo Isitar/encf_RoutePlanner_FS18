1016931804d284e7ceeea48c7652793f6145bdaf
Pascal Lüscher <pascal.luescher@asinfotrack.ch>
2018-02-26 08:20:36 +0100

    Initial commit

---
+++ RoutePlannerConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using Fhnw.Ecnf.RoutePlanner.RoutePlannerLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoutePlannerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Welcome to RoutePlanner (Version {Assembly.GetExecutingAssembly().GetName().Version})");
+            
+            var wayPoint = new Waypoint("Windisch", 47.479319847061966, 8.212966918945312);
+            Console.WriteLine($"{wayPoint.Name}: {wayPoint.Latitude}/{wayPoint.Longitude}");
+            Console.ReadLine();
+        }
+    }
+}
